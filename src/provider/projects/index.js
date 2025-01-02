b0db6b546e902c5f5416253d2ae777bd8fb68551
Abdulhafeez <laxhameedhafeez@gmail.com>
2025-01-02 18:41:47 +0100

    Add ainameit

--- src/provider/projects/index.js
+++ src/provider/projects/index.js
@@ -2,10 +2,22 @@ import mmc from "../../assets/mmc.png";
 import buymore from "../../assets/buymore.png";
 import waki from "../../assets/waki.png";
 import virt from "../../assets/virt.png";
+import ainameit from "../../assets/ainameit.png";
 
 const Projects = [
   {
     id: 1,
+    title: "Business Name Generator",
+    details:
+      "A tool that helps generate business names with available domains using AI in less than a minute.",
+    tools: "Nextjs TypeScript Openai Supabase Zustand",
+    repo: null,
+    url: "https://ainameit.com",
+    img: ainameit,
+    featured: true,
+  },
+  {
+    id: 2,
     title: "MMC Mentorship Web App",
     details:
       "A web application for an annual mentorship program. The platform serves as the central hub where mentees and mentors are seamlessly connected, fostering a productive mentoring experience. The app facilitates the entire mentoring process, allowing mentors to assign tasks to mentees and closely monitor their progress, all within the convenience of the application.",
@@ -16,7 +28,7 @@ const Projects = [
     featured: true,
   },
   {
-    id: 2,
+    id: 3,
     title: "Virtuous Sprout Academy",
     details:
       "An innovative educational website with admin, teacher, and student dashboards, empowering minds through seamless learning experiences. Catering to all schooling activities with user-friendly features for growth and academic excellence.",
@@ -27,7 +39,7 @@ const Projects = [
     featured: true,
   },
   {
-    id: 3,
+    id: 4,
     title: "Wok & Wakis",
     details:
       "Kings family restaurant landing page, with categories of dishes displayed. Items can be added to cart, with necessary specification, and multiple items could be added and removed from cart.",
@@ -38,7 +50,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 4,
+    id: 5,
     title: "E-commerce Web App (BuyMore)",
     details:
       "An e-commerce full-stack web application with the MERN stack. The backend API's implement all the CRUD operations on the various endpoints. It implements JSON Web Token (JWT) to pass identity of authenticated users",
@@ -49,7 +61,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 5,
+    id: 6,
     title: "Where In The World",
     details:
       "A go-to web application for informative details about all the countries in the world. It has a theme-changing feature for sight well-being, a quick search by name, and a region filter. ReactJS and CSS3 were utilized for the development.",
@@ -60,7 +72,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 6,
+    id: 7,
     title: "Dictionary",
     details:
       "A dictionary web application with auto suggestion. It uses Datamuse, random-words-api.vercel.app and api.dictionaryapi.dev/ as backend resources",
@@ -71,7 +83,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 7,
+    id: 8,
     title: "Admin Panel Frontend",
     details: "User management with registration and authentication.",
     tools: "React.js Bootstrap",
@@ -81,7 +93,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 8,
+    id: 9,
     title: "USer Dashboard",
     details: "A dashboard for user management",
     tools: "React.js Redux",
@@ -91,7 +103,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 9,
+    id: 10,
     title: "Admin Panel Backend",
     details:
       "User management with registration and authentication. Non-authenticated users can not have access to the user management (admin panel). Authenticated users has access to the user management table: id, name, e-mail, last login time, registration time, status (active/blocked). With Documentation published on postman docs.",
@@ -102,7 +114,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 10,
+    id: 11,
     title: "Newcore Landing page clone",
     details:
       "A  landing page with different sections, this application was designed in raw Javascript (no libraries), HTML, and CSS. It manipulates DOM elements and relies on event handling",
@@ -113,7 +125,7 @@ const Projects = [
     featured: false,
   },
   {
-    id: 11,
+    id: 12,
     title: "Restful API With Node.js",
     details:
       "A restful API built to provide important information about the countries in the world, with various endpoints. Uses NodeJS (Express) and MongoDB as the database",
